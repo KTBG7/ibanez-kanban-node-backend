ca7b45d480f46a7f464855067885df93b72d82e4
Kevin Ibanez <kevin.ibanez.dev@gmail.com>
2024-06-27 18:33:28 -0500

    Needed to add lax to pass secure cookie for session

--- app.js
+++ app.js
@@ -29,7 +29,7 @@ app.use(session({
     resave: false,
     saveUninitialized: false,
     store: mongoStore,
-    cookie: { secure: true, domain: process.env.UI_DOMAIN }
+    cookie: { secure: true, domain: process.env.UI_DOMAIN, sameSite: "lax" }
 }));
 app.use(authRoutes);
 app.use(userRoutes);
