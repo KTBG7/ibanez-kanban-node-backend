c4663f0e5560dbdd1f274e408efc6e5916712d78
Kevin Ibanez <kevin.ibanez.dev@gmail.com>
2024-06-27 17:30:25 -0500

    updating config for cors

--- utils/CsrfUtil.ts
+++ utils/CsrfUtil.ts
@@ -2,10 +2,9 @@ const doubleCsrfUtilities = require('csrf-csrf').doubleCsrf({
     getSecret: () => "Secret", // A function that optionally takes the request and returns a secret
     cookieName: "__kanban-psifi.x-csrf-token", // The name of the cookie to be used, recommend using Host prefix.
     cookieOptions: {
-        domain: "localhost",
         sameSite : "lax",  // Recommend you make this strict if posible
         path : "/",
-        secure : false,
+        secure : true,
         signed: false
     },
     size: 64,
