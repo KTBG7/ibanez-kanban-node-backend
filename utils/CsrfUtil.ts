cd6b91c300a72fd0cd41a83e1391c15d2ea77437
Kevin Ibanez <kevin.ibanez.dev@gmail.com>
2024-06-27 19:11:38 -0500

    Csrf prod issue, being parsed out test signed config

--- utils/CsrfUtil.ts
+++ utils/CsrfUtil.ts
@@ -2,10 +2,9 @@ const doubleCsrfUtilities = require('csrf-csrf').doubleCsrf({
     getSecret: () => "Secret", // A function that optionally takes the request and returns a secret
     cookieName: "__kanban-psifi.x-csrf-token", // The name of the cookie to be used, recommend using Host prefix.
     cookieOptions: {
-        sameSite : "lax",  // Recommend you make this strict if posible
+        sameSite : "none",  // Recommend you make this strict if posible
         path : "/",
-        secure : true,
-        signed: true
+        secure : true
     },
     size: 64,
     ignoredMethods: ["GET", "HEAD", "OPTIONS"],
