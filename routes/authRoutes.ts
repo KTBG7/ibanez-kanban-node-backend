7352df422d489aec777dc8b8654412d817214bfe
Kevin Ibanez <kevin.ibanez.dev@gmail.com>
2024-07-07 12:28:03 -0500

    I think there was issue once next was called adding to routes directly

--- routes/authRoutes.ts
+++ routes/authRoutes.ts
@@ -1,8 +1,10 @@
+import {findSession} from "../utils/helperFunctions";
+
 const authController = require('../controllers/authController');
 const router = require('express').Router();
 const doubleCsrfProtection = require('../utils/CsrfUtil').doubleCsrfProtection;
-router.post('/login',authController.login);
-router.post('/signup', authController.signup);
+router.post('/login', findSession, authController.login);
+router.post('/signup', findSession, authController.signup);
 router.post('/logout', doubleCsrfProtection, authController.logout);
 
 module.exports = router;
